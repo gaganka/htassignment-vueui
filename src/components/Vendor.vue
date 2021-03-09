80827b6b4bb7aac8f3d998cf57080d8a4c2caf13
gkarla@spplus.onmicrosoft.com <gkalra@spplus.com>
2021-03-09 11:34:44 -0600

    eable disable buttons and post order

--- src/components/Vendor.vue
+++ src/components/Vendor.vue
@@ -24,15 +24,25 @@
       {{inv.name}}<br/>
       {{inv.unitPrice}}<br/>
       <button class="btn btn-outline-success rounded-circle"
-              @click="incrementCount()">+</button>
+              @click="incrementCount($event,inv.unitPrice,inv.productId)" v-show="itemsCount < 6">+</button>
       <hr/>   
     </div>
   </div>
 
+  <div class="row" v-show="itemsCount >= 1">
+    <div class="col-sm-1">$1</div>
+    <div class="col-sm-1">0.25</div>
+    <div class="col-sm-1">0.10</div>
+    <div class="col-sm-1">0.5</div>
+  </div>
+  
+  <hr/>
+
   <div class="row">
-    <div class="col-sm-1"><button class="btn btn-lg btn-success" @click="newOrder()">Buy</button></div>
-    <div class="col-sm-1"><button class="btn btn-lg btn-danger" @click="clearSelection()">Clear</button></div>
+    <div class="col-sm-1"><button class="btn btn-lg btn-success" @click="newOrder()" :disabled='itemsCount === 0'>Buy</button></div>
+    <div class="col-sm-1"><button class="btn btn-lg btn-danger" @click="clearSelection()" :disabled='itemsCount === 0'>Clear</button></div>
     <div class="col-sm-3"><h4>Selected Items {{itemsCount}}</h4></div>
+    <div class="col-sm-3"><h4>Order Total {{orderTotal}}</h4></div>
   </div>
 
 </div>
@@ -40,7 +50,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
 //import {ProductQueryModel} from '../models/product-query-model';
 import QueryDataService from '../services/query-data-service';
 import NewOrderCommandService from '../services/new-order-command-service';
@@ -53,6 +63,7 @@ import { paymentDetails } from "../models/payment-details";
 export default class Vendor extends Vue {
   @Prop() private msg!: string;
   itemsCount = 0;
+  orderTotal = 0;
   public inventory: any[] = [];
   private salesOrder!: salesOrder;
 
@@ -64,6 +75,10 @@ export default class Vendor extends Vue {
     {orderId: 0, productId: 2, quantity: 1, lineItemTotal: 1.75, orderDate: "2021-03-06"},
     {orderId: 0, productId: 3, quantity: 1, lineItemTotal: 1.50, orderDate: "2021-03-06"}
   ];
+
+  private vendOrder: orderDetails[] = [];
+  private orderPayment!: paymentDetails;
+
   //Mock data
   private paymentDetailsMock: paymentDetails = {
     paymentId: 0, amount: 4, amountPaid: 4, paymentDate: "2021-03-06", amountReturned: 0
@@ -80,29 +95,70 @@ export default class Vendor extends Vue {
       });
   }
 
+  @Watch("itemsCount")
+  onitemsCountChanged(){
+    if(this.itemsCount === 6){
+      alert('max selection');
+    }
+  }
+
   mounted(){
     this.retrieveProducts();
     console.log('mounted');
   }
 
-  incrementCount(){
+  incrementCount(event: any,unitPrice: any, productId: any){
     if(this.itemsCount == 6){
       this.itemsCount = 0;
     }
     this.itemsCount++;
+    this.orderTotal += unitPrice;
+
+    const existinRow = this.vendOrder.find(o=>o.productId === productId);
+    const exIndex = this.vendOrder.findIndex(o=>o.productId === productId);
+
+    if(existinRow != undefined){
+      existinRow.quantity +=1;
+      existinRow.lineItemTotal += unitPrice;
+      this.vendOrder[exIndex] = existinRow;
+    }
+    else{
+      this.vendOrder.push({
+        orderId: 0,
+        productId: productId,
+        quantity: 1,
+        orderDate: new Date().toDateString(),
+        lineItemTotal: unitPrice
+      });
+    }
+
+    console.log(JSON.stringify(this.vendOrder));
   }
 
   clearSelection(){
-    this.itemsCount = 0;
+    if(confirm('Clear your selection?')){
+      this.itemsCount = 0;
+      this.orderTotal = 0;
+      this.vendOrder = [];
+    }    
     ///clear the selected items as well
   }
 
   newOrder(){  
+    if(confirm('Place order?')){
 
-    alert('Order created with Mock data');
+    this.orderPayment = {
+      paymentId: 0, 
+      amount: this.orderTotal, 
+      amountPaid: this.orderTotal, 
+      paymentDate: "2021-03-06", 
+      amountReturned: 0
+    }
+
+    //alert('Order created with Mock data');
     this.salesOrder = {
-      orderDetails: this.orderDetailsMock,
-      paymentDetails: this.paymentDetailsMock
+      orderDetails: this.vendOrder,
+      paymentDetails: this.orderPayment
     };
 
     console.log(JSON.stringify(this.salesOrder));
@@ -112,6 +168,10 @@ export default class Vendor extends Vue {
         console.log(JSON.stringify(response.data));
         if(response.data.isSuccess === true){
           alert('Success!!!\nPlease collect your items!!');
+
+          this.itemsCount = 0;
+          this.orderTotal = 0;
+          this.vendOrder = [];          
         }
         else{
           alert('Sorry an error occured');
@@ -120,8 +180,9 @@ export default class Vendor extends Vue {
       })
       .catch((e: any) => {
         console.log(e);
-      });
+      });      
 
+    }
   }
   
 }
